57fa38993000c383ed68b6639cc0dad8bb3c8e8a
AngelBB-1 <angel.acmotos@gmail.com>
2023-06-07 15:44:51 +0200

    aa

--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<head><title>INDEX</title><meta http-equiv="content-type" content="text/html;charset=utf-8" /></head>
-<body>
-	<h1>INDEX de el vostre nom</h1>
-	<h1><a href="consultar.php">consultar</a></h1>
-	<h1><a href="insertar.php">insertar</a></h1>
-	<h1><a href="borrar.php">borrar</a></h1>	
-</body>
-</html>
